92294269ae79ff94eeebfd455d5dce283fd948e6
AdamDotCom <adam@kahtava.com>
2010-03-05 15:53:38 -0700

    Removed :script

--- memcached-on-powershell.ps1
+++ memcached-on-powershell.ps1
@@ -16,17 +16,17 @@ function global:Memcached-Stats($server, $port){
   #-----------------------------------------------------------------
   # Functions local to the script
   #-----------------------------------------------------------------
-  function script:get-Command-Result($command, $stream, $writer){
+  function get-Command-Result($command, $stream, $writer){
     $writer.WriteLine($command)
     $writer.Flush()
     
-    $buffer = (new-object System.Byte[] 1024)
-    $read = $stream.Read($buffer, 0, 1024) 
+    $buffer = (new-object System.Byte[] 4096)
+    $read = $stream.Read($buffer, 0, 4096) 
     
     return ((new-object System.Text.AsciiEncoding).GetString($buffer, 0, $read))
   }
 
-  function script:get-Slabs($results){
+  function get-Slabs($results){
     $slabs = @()
     $regex = 'STAT (?<slabid>.*):'
     foreach ($match in [regex]::matches($results, $regex)) {
@@ -40,7 +40,7 @@ function global:Memcached-Stats($server, $port){
     return $slabs
   }
 
-  function script:write-Slabs($slabs){
+  function write-Slabs($slabs){
     if ($slabs -eq $null){
       write-host 'No slabs found' -fore red
       return
@@ -56,11 +56,11 @@ function global:Memcached-Stats($server, $port){
       
       $result = ((new-object System.Text.AsciiEncoding).GetString($buffer, 0, $read))
       
-      script:write-Cache-Dump $results	
+      write-Cache-Dump $results	
     }
   }
   
-  function script:write-Cache-Dump($results){
+  function write-Cache-Dump($results){
     if ($result.length -eq 5){
       write-host `t 'Empty' -fore red
     }
@@ -68,13 +68,13 @@ function global:Memcached-Stats($server, $port){
     $regex = 'ITEM (?<keyid>.*) \[([0-9].*) b; (?<timestamp>.*) s'
     foreach ($match in [regex]::matches($result, $regex)) {
       #write-host `t 'Age: ' -fore green -no;
-      #write-host (script:convert-From-Unix-Timestamp($match.Groups['timestamp'].Captures[0].Value)) -no
+      #write-host (convert-From-Unix-Timestamp($match.Groups['timestamp'].Captures[0].Value)) -no
       write-host `t 'Key: ' -fore green -no; 
       write-host $match.Groups['keyid'].Captures[0].Value; 
     }
   }
   
-  function script:convert-From-Unix-Timestamp($timestamp){
+  function convert-From-Unix-Timestamp($timestamp){
       $origin = new-object DateTime(1970, 1, 1, 0, 0, 0, 0);
       $origin.AddSeconds($timestamp);
   }
