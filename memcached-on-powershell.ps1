40480b6d88944be317060fc76fea3515b60b8a8c
AdamDotCom <adam@kahtava.com>
2010-03-06 10:25:17 -0700

    Renamed functions

--- memcached-on-powershell.ps1
+++ memcached-on-powershell.ps1
@@ -4,8 +4,11 @@
 #
 # Sample usage:
 #    
-#  Get your Memcached Stats
-#    PS> Memcached-Stats '127.0.0.1' '11411'
+#  See your Memcached Stats
+#    PS> memcached-stats '127.0.0.1' '11211'
+#
+#  Remove your Memcached keys
+#    PS> clear-memcached-items '127.0.0.1' '11211'
 #
 # Adam Kahtava - http://adam.kahtava.com/ - MIT Licensed    
 #
@@ -13,7 +16,7 @@
 
 function global:memcached-on-powershell{
 
-  function script:get-Command-Result($command){
+  function script:get-memcached-command-result($command){
     $writer.WriteLine($command)
     $writer.Flush()
     
@@ -23,7 +26,7 @@ function global:memcached-on-powershell{
     return ((new-object System.Text.AsciiEncoding).GetString($buffer, 0, $read))
   }
 
-  function script:get-Slabs($results){
+  function script:get-memcached-slabs($results){
     $slabs = @()
     $regex = 'STAT (?<slabid>.*):'
     foreach ($match in [regex]::matches($results, $regex)){
@@ -37,7 +40,7 @@ function global:memcached-on-powershell{
     return $slabs
   }
 
-  function script:write-Slabs($slabs){
+  function script:write-memcached-slabs($slabs){
     if ($slabs -eq $null){
       write-host 'No slabs found' -fore red
       return
@@ -45,11 +48,11 @@ function global:memcached-on-powershell{
     foreach ($slab in $slabs){
       write-host 'Stats for Slab: ' $slab -fore yellow
   
-      write-Cache-Dump (get-Command-Result ('stats cachedump {0} 0' -f $slab))
+      write-memcached-cachedump (get-memcached-command-result ('stats cachedump {0} 0' -f $slab))
     }
   }
   
-  function script:write-Cache-Dump($results){
+  function script:write-memcached-cachedump($results){
     if ($results.length -eq 5){
       write-host `t 'Empty' -fore red
     }
@@ -61,7 +64,7 @@ function global:memcached-on-powershell{
     }
   }
   
-  function script:write-Slab-Stats($results){
+  function script:write-memcached-slab-stats($results){
     $regex = 'STAT curr_items (?<totalItems>.*)'
     foreach ($match in [regex]::matches($results, $regex)){
       write-host 'Total items in cache: ' ($match.Groups['totalItems'].Captures[0].Value) -fore red
@@ -69,37 +72,37 @@ function global:memcached-on-powershell{
   }
 }
 
-function global:remove-all-items($server, $port){
-  memcached-on-powershell # Load in memcache symbols
+function global:clear-memcached-items($server, $port){
+  memcached-on-powershell # Load memcache symbols
 
   $socket = new-object System.Net.Sockets.TcpClient($server, $port)
   $stream = $socket.GetStream() 
   $writer = new-object System.IO.StreamWriter $stream 
   
-  $slabs = (get-Slabs (get-Command-Result 'stats slabs'))
+  $slabs = (get-memcached-slabs (get-memcached-command-result 'stats slabs'))
   
   foreach ($slab in $slabs){
     do{
-      $results = get-Command-Result ('stats cachedump {0} 0' -f $slab)
+      $results = get-memcached-command-result ('stats cachedump {0} 0' -f $slab)
       
       $regex = 'ITEM (?<keyid>.*) \['    
       foreach ($match in [regex]::matches($results, $regex)){
         $key = $match.Groups['keyid'].Captures[0].Value
-        $deleteResult = get-Command-Result ('delete {0}' -f $key)
+        $deleteResult = get-memcached-command-result ('delete {0}' -f $key)
       }
     } while ($results.length -gt 5)
   }
   
-  write-Slab-Stats (get-Command-Result 'stats') 
+  write-memcached-slab-stats (get-memcached-command-result 'stats') 
 }
 
-function global:Memcached-Stats($server, $port){
-  memcached-on-powershell # Load in memcache symbols
+function global:memcached-stats($server, $port){
+  memcached-on-powershell # Load memcache symbols
 
   $socket = new-object System.Net.Sockets.TcpClient($server, $port)
   $stream = $socket.GetStream() 
   $writer = new-object System.IO.StreamWriter $stream 
 
-  write-Slab-Stats (get-Command-Result 'stats') 
-  write-Slabs (get-Slabs (get-Command-Result 'stats slabs'))
+  write-memcached-slab-stats (get-memcached-command-result 'stats') 
+  write-memcached-slabs (get-memcached-slabs (get-memcached-command-result 'stats slabs'))
 } 
